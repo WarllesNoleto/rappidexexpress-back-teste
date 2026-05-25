03f8885fbd1e5168bcd7c3960e06d3fa398ec1d1
Warlles Noleto <142702004+WarllesNoleto@users.noreply.github.com>
2026-05-25 13:21:15 -0300

    a

--- src/aiqfome/dto/update-aiqfome-config.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UpdateAiqfomeConfigDto {
-  aiqfomeEnabled?: boolean;
-  aiqfomeStoreId?: string;
-  aiqfomeWebhookSecret?: string;
-  aiqfomeAccessToken?: string;
-  aiqfomeWebhookUrl?: string;
-}
