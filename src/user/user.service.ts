105023ac82ce91c470abd225f0a8d9ed071a6f62
Warlles Noleto <142702004+WarllesNoleto@users.noreply.github.com>
2026-05-31 16:47:05 -0300

    a

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -513,12 +513,7 @@ export class UserService {
             isActive: true,
             'motoboy.id': motoboy.id,
             status: {
-              $in: [
-                StatusDelivery.ONCOURSE,
-                StatusDelivery.COLLECTED,
-                StatusDelivery.ARRIVED_AT_DESTINATION,
-                StatusDelivery.AWAITING_CODE,
-              ],
+              $nin: [StatusDelivery.FINISHED, StatusDelivery.CANCELED],
             },
           };
 
